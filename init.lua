85460a5b3755eb91825963ff18b92b4be08ac968
jgYro <46950626+jgYro@users.noreply.github.com>
2022-05-07 17:58:50 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -26,6 +26,17 @@ end
 -- Output curl request to buffer
 vim.api.nvim_set_keymap('n', '<leader>curl', '<cmd>lua curl_to_buffer()<CR>', options)
 
+function curl_to_py()
+    request = vim.fn.input("Please enter full curl request (command included): ")
+    curl = '"' ..request.. '"'
+    vim.cmd(':enew | .!curlconverter -l python ' .. curl .. '')
+end
+
+vim.api.nvim_set_keymap('n', '<leader>c2p', '<cmd>lua curl_to_py()<CR>', options)
+
+-- NVIM Rest client
+vim.api.nvim_set_keymap('n', '<leader>rest', '<Plug>RestNvim<CR>', options)
+
 -- Run current lua file
 vim.api.nvim_set_keymap('n', '<leader>lua', '<cmd>luafile %<CR>', options)
 
@@ -93,6 +104,38 @@ return require('packer').startup(function()
 
     -- Multi cursor
     use 'mg979/vim-visual-multi'
+
+    -- NVIM Rest Client
+    use {
+        "NTBBloodbath/rest.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("rest-nvim").setup({
+                -- Open request results in a horizontal split
+                result_split_horizontal = false,
+                -- Keep the http file buffer above|left when split horizontal|vertical
+                result_split_in_place = false,
+                -- Skip SSL verification, useful for unknown certificates
+                skip_ssl_verification = false,
+                -- Highlight request on run
+                highlight = {
+                    enabled = true,
+                    timeout = 150,
+                },
+                result = {
+                    -- toggle showing URL, HTTP info, headers at top the of result window
+                    show_url = true,
+                    show_http_info = true,
+                    show_headers = true,
+                },
+                -- Jump to request line on run
+                jump_to_request = false,
+                env_file = '.env',
+                custom_dynamic_variables = {},
+                yank_dry_run = true,
+            })
+        end
+    }
 
     --
     --
